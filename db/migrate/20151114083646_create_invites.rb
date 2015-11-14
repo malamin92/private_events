dd89706488a975c4e59994687d125cdb891ed3f9
Mo Amin <m.alamin92@gmail.com>
2015-11-14 04:13:45 -0500

    roolback changes

--- db/migrate/20151114083646_create_invites.rb
+++
@@ -1,8 +0,0 @@
-class CreateInvites < ActiveRecord::Migration
-  def change
-    create_table :invites do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111230401) do
+ActiveRecord::Schema.define(version: 20151114082524) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
