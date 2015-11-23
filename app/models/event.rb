83d5c301a76e57e098e1e42c2e607abd15f800fc
Mo Amin <m.alamin92@gmail.com>
2015-11-23 05:42:15 -0500

    finish mvp

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,17 @@
 class Event < ActiveRecord::Base
+
+	scope :past, -> { where("date < ?", Time.now.strftime("%m/%d/%Y")) }
+	scope :future, -> { where("date > ?", Time.now.strftime("%m/%d/%Y")) }
+
 	belongs_to :creator, :class_name => "User"
 	has_many :invites, :foreign_key => :attending_event_id
 	has_many :attendees, :through => :invites, :source => :attending_guest
+
+	# def self.past
+	# 	self.where("date < ?", Time.now.strftime("%m/%d/%Y"))
+	# end
+
+	# def self.future
+	# 	self.where("date > ?", Time.now.strftime("%m/%d/%Y"))
+	# end
 end
